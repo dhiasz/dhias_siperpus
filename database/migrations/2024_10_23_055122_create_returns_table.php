ac35a26f55150936cf85b87bbebb8f2ef3d0a846
dhiasz <dhiasa01@gmail.com>
2024-10-23 13:02:00 +0700

    databaru database

---
+++ database/migrations/2024_10_23_055122_create_returns_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('returns', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('loan_detail_id');
+            $table->boolean('change');
+            $table->integer('amount');
+            $table->timestamps();
+            
+            
+            $table->foreign('loan_detail_id')->references('id')->on('loan_details')
+                    ->onUpdate('cascade')->onDelete('cascade');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+
+        Schema::create('returns', function (Blueprint $table) {
+            $table->dropConstrainedForeignId('loan_detail_id');
+        });
+        Schema::dropIfExists('returns');
+    }
+};
